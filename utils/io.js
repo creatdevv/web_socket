f025533ef140b6f563fe6a53c15ade164e02a8b9
devlara <createdevlara@gmail.com>
2024-07-10 19:03:40 +0900

    socket 연결 끊기도 만들기

--- utils/io.js
+++ utils/io.js
@@ -2,6 +2,11 @@ module.exports=function() {
     // io 와 관련된 모든 일~~, index에 require 추가!
 io.on("connection", async(socket)=> {
     console.log("client is connected, socket.id");
-});
 
+
+    // socket 연결 끊어주는것도 만들기!
+    socket.on("disconnect", () => {
+        console.log("user is disconnected");
+     });
+   });
 };
